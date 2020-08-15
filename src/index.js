997187cb4b3b67b580d201eff7eaa5ae68c1ca40
Raphael <raphaelmalinski@gmail.com>
2020-08-14 21:52:03 -0300

    Adding requestRobots to redux with redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkWiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = createStore(rootReducer, applyMiddleware(thunkWiddleware, logger));
 
 ReactDOM.render(
                 <Provider store={store} >
